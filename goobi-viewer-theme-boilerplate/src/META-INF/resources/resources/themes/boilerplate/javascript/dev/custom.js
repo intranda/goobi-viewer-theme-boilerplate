cdfbe3ab9dec09e88a2c5a35705a6dfad3a76b83
Marc Lettau-Poelchen <marc.lettau-poelchen@intranda.com>
2018-07-13 16:07:58 +0200

    TBP: added subtheme variable and js clean up

--- goobi-viewer-theme-boilerplate/src/META-INF/resources/resources/themes/boilerplate/javascript/dev/custom.js
+++ goobi-viewer-theme-boilerplate/src/META-INF/resources/resources/themes/boilerplate/javascript/dev/custom.js
@@ -2,29 +2,6 @@
  * Custom JavaScript for "boilerplate"
  ****************************************************************************************/
 /**
- * Method to move DOM-Elements to another place.
- * 
- * @method manipulateDom
- */
-function manipulateDom() {
-    $( '#widgetSearchField' ).detach().insertAfter( '.page-content__close-sidebar' );
-    $( '#changeLocal' ).detach().insertAfter( '.page-content__close-sidebar' );
-}
-
-/**
- * Method to reset DOM-Elements.
- * 
- * @method resetDom
- */
-function resetDom() {
-    var searchField = $( '#widgetSearchField' ).detach();
-    var changeLocal = $( '#changeLocal' ).detach();
-    
-    $( '.page-header__search' ).append( searchField );
-    $( '.page-header__language' ).append( changeLocal );
-}
-
-/**
  * Method to set the content height to a min-height, equal to the viewport height.
  */
 function setContentHeight() {
@@ -63,31 +40,7 @@ $( document ).ready( function() {
     // set content height to window height
     // setContentHeight(); --> METHODE AN LAYOUT ANPASSEN!
     
-    // off canvas
-    $( '[data-toggle="offcanvas"]' ).off().click( function() {
-        $( this ).addClass( 'in' );
-        $( '#sidebar' ).addClass( 'in' );
-        $( '.offcanvas-fade' ).addClass( 'in' );
-    } );
-    $( '[data-toggle="close-sidebar"]' ).click( function() {
-        $( '#sidebar' ).removeClass( 'in' );
-        $( '[data-toggle="offcanvas"]' ).removeClass( 'in' );
-        $( '.offcanvas-fade' ).removeClass( 'in' );
-    } );
-    
-    // change position of search and language
-    if ( window.matchMedia( '(max-width: 768px)' ).matches ) {
-        manipulateDom();
-    }
-    $( window ).on( 'resize orientationchange', function() {
-        // setContentHeight(); --> METHODE AN LAYOUT ANPASSEN!
-        if ( window.matchMedia( '(max-width: 768px)' ).matches ) {
-            // setContentHeight(); --> METHODE AN LAYOUT ANPASSEN!
-            manipulateDom();
-        }
-        else {
-            // setContentHeight(); --> METHODE AN LAYOUT ANPASSEN!
-            resetDom();
-        }
-    } );
+    // mobile view manipulations
+    if ( window.matchMedia( '(max-width: 768px)' ).matches ) {}
+    $( window ).on( 'resize orientationchange', function() {} );
 } );
