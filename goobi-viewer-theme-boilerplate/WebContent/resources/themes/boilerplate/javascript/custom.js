83894e6fb73e4e09e930504b19ac4a406c559a9e
Jan Günther <jan.guenther@intranda.com>
2021-04-01 10:35:07 +0200

    changelog to 21.03

--- goobi-viewer-theme-boilerplate/WebContent/resources/themes/boilerplate/javascript/custom.js
+++ goobi-viewer-theme-boilerplate/WebContent/resources/themes/boilerplate/javascript/custom.js
@@ -1,6 +1,33 @@
 /*****************************************************************************************
  * Custom JavaScript for "boilerplate"
  ****************************************************************************************/
+
+/*
+ initSliders may be used to add new cms slider styles (swiper configuration objects) to the 
+ list of availabe slider styles, or alter existing ones. See the commented out examples below for each use case
+ This method MUST be called before document.ready for them to show up in the styles dropdown in the cms backend
+*/
+function initSliders() {
+	//update swiper config with
+	    // var partialConfig = {
+        // swiperConfig: {
+        //        loop: true,
+        //        slidesPerView: 2
+        //    }
+        // }
+	// viewerJS.slideshows.update('styleName', partialConfig);
+        // add new swiper config
+        // var config = {
+        //    swiperConfig: {
+        //        slidesPerView: 3,
+        //        spaceBetween: 50,
+        //        loop: true
+        //    }
+        //}
+	// viewerJS.slideshows.set('myNewStyleName', config);
+}
+initSliders();
+
 /**
  * Method to set the content height to a min-height, equal to the viewport height.
  */
